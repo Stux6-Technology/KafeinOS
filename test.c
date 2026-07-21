c71f89e6df03422754c7c2a1b0438eec1c9deb80
Alperen ERKAN <erkanalperen54@gmail.com>
2026-07-21 13:35:14 +0300

    kernel update

---
+++ test.c
@@ -0,0 +1,38 @@
+
+/*
+ * @file: kernel.c
+ * @name: KafeinOS Test Kernel
+ * @authors: Alperen ERKAN & M. Ekrem Güler
+ * @mail: stux6.team@gmail.com
+ * 
+ * @bug: X
+ * @note: X
+ * @Thank: Linus Torvalds (2026)
+*/
+
+#include <source/kafein.h>
+#include <source/kernel.h>
+
+#define KAFEIN_H
+#define KERNEL_H
+
+#ifndef KERNEL_T
+static inline KERNEL_H
+#endif
+
+unsigned char boot[512];
+
+
+void kernel_inline(unsigned char boot, char *boot_img)
+{
+
+} 
+
+union 
+{
+    /* data */
+};
+
+/*
+ * @note
+*/
